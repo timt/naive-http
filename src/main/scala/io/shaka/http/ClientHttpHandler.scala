3873de9796080c5cd86227737a9a101d818ece84
Tim Tennant <ttennant@gmail.com>
2014-05-14 08:46:02 +0100

    Refactor for readability

--- src/main/scala/io/shaka/http/ClientHttpHandler.scala
+++ src/main/scala/io/shaka/http/ClientHttpHandler.scala
@@ -9,18 +9,10 @@ import HttpHeader.httpHeader
 import scala.collection.JavaConversions.mapAsScalaMap
 import scala.collection.JavaConversions.collectionAsScalaIterable
 
-
-
-//TODO model header parameters as Map[String,List[String]]
 class ClientHttpHandler extends HttpHandler {
+
   override def apply(request: Request): Response = {
-    //TODO handle proxies @see URL.openConnection(Proxy)
-    val connection = new URL(request.url).openConnection().asInstanceOf[HttpURLConnection]
-    val timeoutInMillis = 0
-    connection.setUseCaches(false)
-    connection.setConnectTimeout(timeoutInMillis)
-    connection.setReadTimeout(timeoutInMillis)
-    connection.setInstanceFollowRedirects(false)
+    val connection = createConnection(request.url)
     connection.setRequestMethod(request.method.name)
     request.headers.foreach {
       header =>
@@ -31,7 +23,10 @@ class ClientHttpHandler extends HttpHandler {
         connection.setDoOutput(true)
         connection.getOutputStream.write(entity.content)
     }
+    buildResponse(connection)
+  }
 
+  def buildResponse(connection: HttpURLConnection) = {
     val s = status(connection.getResponseCode, connection.getResponseMessage)
     val is = if(s.code >=400) connection.getErrorStream else connection.getInputStream
     val entity = Entity(Source.fromInputStream(is).map(_.toByte).toArray)
@@ -42,6 +37,16 @@ class ClientHttpHandler extends HttpHandler {
       headers = headers,
       entity = Some(entity)
     )
+  }
+
+  def createConnection(url: Url) = {
+    val connection = new URL(url).openConnection().asInstanceOf[HttpURLConnection]
+    val timeoutInMillis = 0
+    connection.setUseCaches(false)
+    connection.setConnectTimeout(timeoutInMillis)
+    connection.setReadTimeout(timeoutInMillis)
+    connection.setInstanceFollowRedirects(false)
+    connection
   }
 
 }

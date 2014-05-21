f3147232adb21f594119b0418bd80e1bcf8a401e
travis-ci <travis@travis-ci.org>
2014-05-21 08:28:29 +0100

    Ensure only publish timt/naive-http, not forks, or pull requests and only merges to master

--- publish/publish.sh
+++ publish/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#if [ "$TRAVIS_REPO_SLUG" == "timt/naive-http" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "timt/naive-http" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
     cd $TRAVIS_BUILD_DIR/publish
 
     git config --global user.email "travis@travis-ci.org"
@@ -11,4 +11,4 @@
     git commit -m "Lastest naive-http lib on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to repo"
     git push -fq origin master > /dev/null
     echo -e "Published naive-http lib to repo.\n"
-#fi
+fi

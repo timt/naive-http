558e8439fe15addfa91b17062121b66e10c0d498
travis-ci <travis@travis-ci.org>
2014-05-31 17:04:24 +0100

    Added convenience/builder methods to Response

--- src/main/scala/io/shaka/http/Response.scala
+++ src/main/scala/io/shaka/http/Response.scala
@@ -1,5 +1,15 @@
 package io.shaka.http
 
 import io.shaka.http.Status.OK
+import io.shaka.http.HttpHeader.CONTENT_TYPE
 
-case class Response(status: Status = OK, headers: Headers = Headers.Empty, entity: Option[Entity] = None)
+case class Response(status: Status = OK, headers: Headers = Headers.Empty, entity: Option[Entity] = None){
+
+  def entity(content: String) = copy(entity = Some(Entity(content)))
+
+  def header(header: HttpHeader, value: String) = copy(headers = (header, value) :: headers)
+
+  def contentType(value: String) = header(CONTENT_TYPE, value)
+
+  def contentType(value: ContentType) = header(CONTENT_TYPE, value.value)
+}

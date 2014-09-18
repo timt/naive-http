b1034716e83082f03621b035fc0c1cf6afa8f9be
Tim Tennant <ttennant@gmail.com>
2014-09-18 22:29:18 +0100

    Added convenience method respond(content: Array[Byte])

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Add the following lines to your build.sbt
 
     resolvers += "Tim Tennant's repo" at "http://dl.bintray.com/timt/repo/"
 
-    libraryDependencies += "io.shaka" %% "naive-http" % "51"
+    libraryDependencies += "io.shaka" %% "naive-http" % "57"
 
 Start hacking
 
--- src/main/scala/io/shaka/http/Response.scala
+++ src/main/scala/io/shaka/http/Response.scala
@@ -5,24 +5,18 @@ import io.shaka.http.HttpHeader.CONTENT_TYPE
 
 object Response {
   def respond(content: String): Response = Response().entity(content)
+  def respond(content: Array[Byte]): Response = Response().entity(content)
 }
 
-case class Response(status: Status = OK, headers: Headers = Headers.Empty, entity: Option[Entity] = None){
-
+case class Response(status: Status = OK, headers: Headers = Headers.Empty, entity: Option[Entity] = None) {
   def status(newStatus: Status) = copy(status = newStatus)
-
   def header(header: HttpHeader, value: String) = copy(headers = (header, value) :: headers)
-
   def header(header: HttpHeader) = headers.find(_._1 == header).map(_._2)
-
   def contentType(value: String) = header(CONTENT_TYPE, value)
-
   def contentType(value: ContentType) = header(CONTENT_TYPE, value.value)
-
   def contentType = header(CONTENT_TYPE).map(ContentType.contentType)
-
   def entity(content: String): Response = copy(entity = Some(Entity(content)))
-
+  def entity(content: Array[Byte]): Response = copy(entity = Some(Entity(content)))
   def entityAsString: String = entity match {
     case Some(value) => value.toString
     case _ => throw new RuntimeException("There is no entity in this response! Consider using response.entity:Option[Entity] instead.")

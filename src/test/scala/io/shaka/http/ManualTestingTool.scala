55574bac46c71d286e331d97aa56c9de90613f19
jcaraballo <git@caraballo.eu>
2015-06-11 01:00:15 +0100

    Update to use client-truststore from naive-http-server

--- src/test/scala/io/shaka/http/ManualTestingTool.scala
+++ src/test/scala/io/shaka/http/ManualTestingTool.scala
@@ -6,7 +6,7 @@ import io.shaka.http.Request.GET
 // TODO Convert into automated test once code dependency has been sorted out
 object ManualTestingTool extends App {
   val response = Http.http(GET("https://127.0.0.1:7878/foo"))(httpsConfig = Some(HttpsConfig(
-    TrustServersByTrustStore("client-truststore.jks", "password"),
+    TrustServersByTrustStore("../naive-http-server/client-truststore.jks", "password"),
     UseKeyStore("src/test/resources/certs/keystore-testing-client.jks", "password"))
   ))
   println(response)

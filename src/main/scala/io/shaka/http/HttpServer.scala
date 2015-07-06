9913f068abc8c9b39d383e7bf1c28406ba2e0390
jcaraballo <git@caraballo.eu>
2015-07-06 18:13:43 +0100

    Support https with mutual auth
    Support for more usual server-only certificate SSL should be added too
    (Applied from naive-http-server mutual-ssl-auth and then amended to deal with id clashes)

--- src/main/scala/io/shaka/http/HttpServer.scala
+++ src/main/scala/io/shaka/http/HttpServer.scala
@@ -1,18 +1,21 @@
 package io.shaka.http
 
+import java.io.FileInputStream
 import java.net.InetSocketAddress
+import java.security.{SecureRandom, KeyStore}
+import javax.net.ssl.{SSLParameters, TrustManagerFactory, KeyManagerFactory, SSLContext}
 
 import com.sun.net.httpserver.spi.HttpServerProvider
-import com.sun.net.httpserver.{HttpServer => SunHttpServer}
+import com.sun.net.httpserver.{HttpServer => SunHttpServer, HttpsParameters, HttpsConfigurator, HttpsServer}
 import io.shaka.http.Http.HttpHandler
 import io.shaka.http.HttpServer.ToLog
 import io.shaka.http.Response.respond
 import io.shaka.http.Status.NOT_FOUND
 
-class HttpServer(private val usePort: Int = 0, otherLog: ToLog) {
+class HttpServer(private val usePort: Int = 0, otherLog: ToLog, maybeHttps: Option[HttpsConfig] = None) {
   import HttpServer.createServer
 
-  val server = createServer(usePort)
+  val server = createServer(usePort, maybeHttps)
   server.setExecutor(null)
   server.createContext("/", new SunHttpHandlerAdapter((req) => respond("No handler defined!").status(NOT_FOUND)))
 
@@ -46,9 +49,42 @@ object HttpServer {
   def apply(port: Int): HttpServer = new HttpServer(port, printlnLog)
   def apply(handler: HttpHandler, port: Int = 0, log: ToLog = printlnLog): HttpServer = new HttpServer(port, log).handler(handler)
 
-  private def createServer(requestedPort: Int): SunHttpServer = {
+  def https(httpsConfig: HttpsConfig, port: Int = 0) = new HttpServer(port, printlnLog, Some(httpsConfig))
+
+  private def createServer(requestedPort: Int, maybeHttps: Option[HttpsConfig]): SunHttpServer = {
     val address: InetSocketAddress = new InetSocketAddress(requestedPort)
     val httpServerProvider: HttpServerProvider = HttpServerProvider.provider()
-    httpServerProvider.createHttpServer(address, 0)
+
+    maybeHttps match {
+      case None => httpServerProvider.createHttpServer(address, 0)
+      case Some(HttpsConfig(ksConfig, tsConfig)) =>
+
+        val ks: KeyStore = KeyStore.getInstance("JKS")
+        ks.load(new FileInputStream(ksConfig.path), ksConfig.password.toCharArray)
+        val kmf = KeyManagerFactory.getInstance("SunX509")
+        kmf.init(ks, ksConfig.password.toCharArray)
+
+        val ts: KeyStore = KeyStore.getInstance("JKS")
+        ts.load(new FileInputStream(tsConfig.path), tsConfig.password.toCharArray)
+        val tmf: TrustManagerFactory = TrustManagerFactory.getInstance("SunX509")
+        tmf.init(ts)
+
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(kmf.getKeyManagers, tmf.getTrustManagers, new SecureRandom())
+
+        val server: HttpsServer = httpServerProvider.createHttpsServer(address, 0)
+
+        server.setHttpsConfigurator(new HttpsConfigurator(sslContext){
+          override def configure(httpsParameters: HttpsParameters) = {
+            val sslParameters: SSLParameters = getSSLContext.getDefaultSSLParameters
+            sslParameters.setNeedClientAuth(true)
+            httpsParameters.setSSLParameters(sslParameters)
+          }
+        })
+        server
+    }
   }
 }
+
+case class PathAndPassword(path: String, password: String) // TODO: Use client's
+case class HttpsConfig(keyStoreConfig: PathAndPassword, trustStoreConfig: PathAndPassword)
--- src/test/scala/io/shaka/http/ManualTestingTool.scala
+++ src/test/scala/io/shaka/http/ManualTestingTool.scala
@@ -1,13 +1,22 @@
 package io.shaka.http
 
-import io.shaka.http.Https._
+import io.shaka.http.Https.{UseKeyStore, TrustServersByTrustStore}
 import io.shaka.http.Request.GET
 
 // TODO Convert into automated test once code dependency has been sorted out
-object ManualTestingTool extends App {
-  val response = Http.http(GET("https://127.0.0.1:7878/foo"))(httpsConfig = Some(HttpsConfig(
+object ManualTestingClientTool extends App {
+  val response = Http.http(GET("https://127.0.0.1:7878/foo"))(httpsConfig = Some(io.shaka.http.Https.HttpsConfig(
     TrustServersByTrustStore("../naive-http-server/client-truststore.jks", "password"),
     UseKeyStore("src/test/resources/certs/keystore-testing-client.jks", "password"))
   ))
   println(response)
+}
+
+// TODO Convert into automated test once code dependency has been sorted out
+object ManualTestingServerTool extends App {
+  private val ksConfig = PathAndPassword("naive-http/src/test/resources/certs/keystore-testing.jks", "password")
+  private val tsConfig = PathAndPassword("../naive-http/server-truststore.jks", "password")
+
+  val server = HttpServer.https(io.shaka.http.HttpsConfig(ksConfig, tsConfig), 7878).start()
+  println("Mutual SSL auth http server started at https://localhost:" + server.port)
 }

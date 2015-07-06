2fa42bef58de6b310b26493079692fa090af3232
jcaraballo <git@caraballo.eu>
2015-07-06 19:10:46 +0100

    Convert manual test into automated one

---
+++ src/test/scala/io/shaka/http/MutualSslAuthSpec.scala
@@ -0,0 +1,27 @@
+package io.shaka.http
+
+import io.shaka.http.Https.{TrustServersByTrustStore, UseKeyStore}
+import io.shaka.http.Request.GET
+import io.shaka.http.Response.respond
+import io.shaka.http.Status.OK
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+
+
+class MutualSslAuthSpec extends FunSuite with BeforeAndAfterEach {
+  test("Client can connect to server doing mutual SSL auth") {
+    val server = HttpServer.https(io.shaka.http.HttpsConfig(
+      keyStoreConfig = PathAndPassword("src/test/resources/certs/keystore-testing.jks", "password"),
+      trustStoreConfig = PathAndPassword("server-truststore.jks", "password"))).handler(_ => respond("Hello world")).start()
+    try {
+      val response = Http.http(GET(s"https://127.0.0.1:${server.port}/foo"))(httpsConfig = Some(io.shaka.http.Https.HttpsConfig(
+        TrustServersByTrustStore("client-truststore.jks", "password"),
+        UseKeyStore("src/test/resources/certs/keystore-testing-client.jks", "password"))
+      ))
+
+      assert((response.status, response.entity.map(_.toString)) === (OK, Some("Hello world")))
+
+    } finally {
+      server.stop()
+    }
+  }
+}

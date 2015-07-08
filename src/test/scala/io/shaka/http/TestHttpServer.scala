5898def7f67e8a1072463f4d10a43d3204394ac2
jcaraballo <git@caraballo.eu>
2015-07-08 23:38:55 +0100

    Undo: Make (Unfiltered) test http server require client certificate
    As it was neither working nor being used

--- src/test/scala/io/shaka/http/TestHttpServer.scala
+++ src/test/scala/io/shaka/http/TestHttpServer.scala
@@ -1,7 +1,6 @@
 package io.shaka.http
 
 import unfiltered.filter.Planify
-import unfiltered.jetty.{Ssl, Server}
 import unfiltered.request.{Path, Seg}
 import unfiltered.response._
 
@@ -13,31 +12,15 @@ object TestHttpServer {
     server.stop()
   }
   def withHttpsServer(block: TestHttpServer => Unit) = {
-    val trustStore = PathAndPassword("src/test/resources/certs/truststore-testing.jks", "password")
-    val keyStore = PathAndPassword("src/test/resources/certs/keystore-testing.jks", "password")
-    val server = new TestHttpServer(HttpsWithMutualAuth(unfiltered.util.Port.any, "127.0.0.1", keyStore, trustStore)).start()
+    System.setProperty("jetty.ssl.keyStore", "src/test/resources/certs/keystore-testing.jks")
+    System.setProperty("jetty.ssl.keyStorePassword", "password")
+    val server = new TestHttpServer(unfiltered.jetty.Https.anylocal).start()
     block(server)
     server.stop()
+    System.clearProperty("jetty.ssl.keyStore")
+    System.clearProperty("jetty.ssl.keyStorePassword")
   }
   def apply() = new TestHttpServer().start()
-}
-
-trait Ssl2 { self: Ssl =>
-  override val keyStore: String = ""
-  override val keyStorePassword: String = ""
-}
-
-case class HttpsWithMutualAuth(port: Int, host: String, ks: PathAndPassword, ts: PathAndPassword) extends Server with Ssl  {
-  override lazy val keyStore: String = ks.path
-  override lazy val keyStorePassword: String = ks.password
-
-  sslContextFactory.setTrustStore(ts.path)
-  sslContextFactory.setTrustStorePassword(ts.password)
-
-  type ServerBuilder = HttpsWithMutualAuth
-  val url = "https://%s:%d/" format (host, port)
-  def sslPort = port
-  sslConn.setHost(host)
 }
 
 class TestHttpServer(private val server:unfiltered.jetty.Server = unfiltered.jetty.Http.anylocal) {

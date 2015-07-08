9361fe55c01d18eade85c427d68aeefd70c0c481
jcaraballo <git@caraballo.eu>
2015-07-08 23:59:36 +0100

    Move trust stores to test certs dir

--- src/test/scala/io/shaka/http/TestCerts.scala
+++ src/test/scala/io/shaka/http/TestCerts.scala
@@ -13,7 +13,7 @@ object TestCerts {
   // [...]
   // naive-testing-client, 10-Jun-2015, trustedCertEntry,
   // Certificate fingerprint (MD5): DC:22:BD:9D:C4:7D:3E:A8:80:3E:BF:69:D9:8B:03:20
-  val trustStoreWithClientCert: PathAndPassword = PathAndPassword("server-truststore.jks", "password")
+  val trustStoreWithClientCert: PathAndPassword = PathAndPassword("src/test/resources/certs/server-truststore.jks", "password")
 
 
   // For client
@@ -22,7 +22,7 @@ object TestCerts {
   // [...]
   // naive-testing-server, 11-Jun-2015, trustedCertEntry,
   // Certificate fingerprint (MD5): 90:75:3A:10:1C:64:18:86:B4:EC:36:2D:40:2F:69:F6
-  val trustStoreWithServerCert: PathAndPassword = PathAndPassword("client-truststore.jks", "password")
+  val trustStoreWithServerCert: PathAndPassword = PathAndPassword("src/test/resources/certs/client-truststore.jks", "password")
 
   // $ keytool -list -keystore keystore-testing-client.jks -storepass password
   // [...]

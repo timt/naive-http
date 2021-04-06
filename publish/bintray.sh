8c7c8dbff53da44e655fafe4d2c8d9fa3a711461
Tim Tennant <tim.tennant@nivohub.com>
2021-04-06 12:12:41 +0100

    Removing bintry and travis-ci from repo

--- publish/bintray.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.bintray
-eval "echo \"$(< ./publish/bintray.template)\"" > ~/.bintray/.credentials
-openssl des3 -d -salt -in ./publish/sonatype.asc.enc -out ./publish/sonatype.asc -k "$SECRET"
